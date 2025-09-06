42b356aff22b9936875ad0f17827d5240065f310
Ashish-raj05 <136631477+Ashish-raj05@users.noreply.github.com>
2025-09-07 00:13:01 +0530

    2nd commit

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -1,4 +1,4 @@
-import { useUser } from '@clerk/clerk-react'
+import { useClerk, useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AiToolsData } from '../assets/assets'
@@ -7,6 +7,7 @@ const AiTools = () => {
 
     const navigate = useNavigate()
     const {user} = useUser()
+    const {openSignIn} = useClerk()
 
   return (
     <div className='px-4 sm:px-20 xl:px-32 my-24'>
@@ -18,7 +19,7 @@ const AiTools = () => {
         <div className='flex flex-wrap mt-10 justify-center'>
             {AiToolsData.map((tool, index)=>(
                 <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition-all duration-300 cursor-pointer'
-                 onClick={()=> user && navigate(tool.path)}>
+                 onClick={() => (!user ? openSignIn() : navigate(tool.path))}>
                 <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style= {{background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool. bg.to})`}}/>
                 <h3 className='mt-6 mb-3 text-lg font-semibold'>{tool.title}</h3>
                 <p className='text-gray-400 text-sm max-w-[95%]'>{tool.description}</p>
