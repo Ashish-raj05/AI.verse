4fc992bd8d8ec285e9190e5204ab8de507fdf0be
Ashish Raj <136631477+Ashish-raj05@users.noreply.github.com>
2025-12-10 16:36:58 +0530

    Upgrade AI model

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -24,7 +24,7 @@ export const generateArticle = async (req, res) => {
         }
 
     const response = await AI.chat.completions.create({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     messages: [
         {
             role: "user",
@@ -67,7 +67,7 @@ export const generateBlogTitle = async (req, res) => {
         }
 
     const response = await AI.chat.completions.create({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     messages: [
         {
             role: "user",
@@ -210,7 +210,7 @@ export const resumeReview = async (req, res) => {
         const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas for improvement. Resume Content:\n\n${pdfData.text}`
 
          const response = await AI.chat.completions.create({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     messages: [
         {
             role: "user",
